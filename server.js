6117f563efd74971a8beb99a3c735e6e26a7db98
Omri <ogolan3589@gmail.com>
2020-12-27 23:44:24 +0000

    save country json array for initialization

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
-const express=require('express')
-const app=express()
-const http_port=8080
+const express = require('express')
+const app = express()
+const fetch = require('node-fetch')
+const fs = require('fs')
+const path = require('path')
+
+const http_port = 8080
 
 app.use(express.text())
 app.use(express.json())
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('works')
 })
 
-app.get('/api/text-test', (req, res)=>{
-    res.send('text works')
+app.get('/api/getAll', (req, res) => {
+
+    var data = fs.readFileSync(path.join(__dirname + '/data/country-store.json'))
+    var countries = []
+    if (data.length == 0) {
+        fetch('https://covid19.mathdro.id/api/countries')
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                if (!data.error) {
+                    data.countries.map(country => {
+                        var countryData = {}
+                        countryData.name = country.name
+                        countries.push(countryData)
+                        fs.writeFileSync(path.join(__dirname + '/data/country-store.json'), JSON.stringify(countries), (error) => {
+                            if (error)
+                                console.log(error)
+                        })
+                    })
+                }
+            })
+    }
+
 })
 
-app.get('/api/json-test', (req, res)=>{
-    res.send({message:'json works'})
-})
-
-app.listen(http_port,()=>{
+app.listen(http_port, () => {
     console.log('listening')
 })
