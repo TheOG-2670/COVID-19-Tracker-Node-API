05e5feec1a62dd1d7da054a156cde6442803ce6e
Omri <ogolan3589@gmail.com>
2020-12-30 03:29:45 +0000

    set up separate calls to covid api for fetching global data and country data; removed testing code from covid data retrieval function

--- data-service.js
+++ data-service.js
@@ -11,7 +11,11 @@ module.exports.initData = function()
             })
             .then(data => {
                 if (!data.error) {
-                    data.countries.map(country => {
+                    
+                    countries.push({
+                        name:"Global"
+                    })
+                    data.countries.forEach(country => {
                         var countryData = {}
                         countryData.name = country.name
                         countries.push(countryData)
@@ -22,52 +26,45 @@ module.exports.initData = function()
                     })
                 }
             })
-        }
+}
+
 
         //.splice to replace data in array -> parameters: index, number of elements, object
         //.find to find and return object/element in array -> parameters: lambda condition
         //.indexOf to find index of specific object/element in array -> parameters: element/object
-module.exports.updateCovidData = function (countries) {
+module.exports.updateCovidData = async function (countries) {
 
-        countries.forEach(country=>{
-            fetch('https://covid19.mathdro.id/api/countries/' + country.name)
-            .then(response=>{
-                return response.json()
-            })
-            if(!data.error)
-            {
-                
-                var countryData = {};
-                countryData.name = country.name
-                countryData.confirmed = data.confirmed.value
-                countryData.deaths = data.deaths.value
-                countryData.recovered = data.recovered.value
-                        
-                countries.splice(i, 1, countryData)
-                fs.writeFileSync(path.join(__dirname + '/data/country-store.json'), JSON.stringify(countries, null, 2), (error) => {
-                    if (error)
-                        console.log(error)
-                })
-            }
+    var globalData = await fetch('https://covid19.mathdro.id/api')
+    var globalResponse = await globalData.json();
+
+        var selectedCountry = countries.find(c=>c.name==="Global")
+        selectedCountry.confirmed=globalResponse.confirmed.value
+        selectedCountry.deaths=globalResponse.deaths.value
+        selectedCountry.recovered=globalResponse.recovered.value
+        countries.splice(countries.indexOf(selectedCountry), 1, selectedCountry)
+
+
+        for(var i=0; i < countries.length; i++)
+        {
+                var countryData = await fetch('https://covid19.mathdro.id/api/countries/' + countries[i].name)
+                var countryResponse = await countryData.json();
+                if(!countryResponse.error)
+                {
+                            
+                    countries[i].confirmed=countryResponse.confirmed.value
+                    countries[i].deaths=countryResponse.deaths.value
+                    countries[i].recovered=countryResponse.recovered.value
+                    countries.splice(i, 1, countries[i])
+                    
+                }
+        }
+
+        fs.writeFileSync(path.join(__dirname + '/data/country-store.json'), JSON.stringify(countries, null, 2), (error) => {
+            if (error)
+                console.log(error)
         })
 }
 
 module.exports.getCovidData = function () {
-
-    //TESTING CODE
-    var countries=JSON.parse(fs.readFileSync(path.join(__dirname + "/data/country-store.json")))
-    var a = countries.find(c=>c.name==="Afghanistan")
-    var b = countries.find(c=>c.name==="Albania")
-    
-    a.confirmed+=1
-    b.confirmed+=1
-    countries.splice(countries.indexOf(a), 1, a)
-    countries.splice(countries.indexOf(b), 1, b)
-    fs.writeFileSync(path.join(__dirname + '/data/country-store.json'), JSON.stringify(countries, null, 2), (error) => {
-        if (error)
-            console.log(error)
-    })
-    //
-    
     return JSON.parse(fs.readFileSync(path.join(__dirname + "/data/country-store.json")))
 }
